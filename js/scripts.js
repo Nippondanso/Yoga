72570081426e42e2cfd29454f04cd082335ab880
Maksym Kopaiev <mkopaev@gmail.com>
2020-02-26 19:24:01 +0200

    Изменена форма для получения телефона: переписана с использованием Promise()

--- js/scripts.js
+++ js/scripts.js
@@ -127,39 +127,82 @@ window.addEventListener('DOMContentLoaded', function () {
 
     statusMessage.classList.add('status');
 
+    function sendForm(form) {
+        form.addEventListener('submit', function (event) {
+            event.preventDefault();
+            form.appendChild(statusMessage);
 
-    form.addEventListener('submit', function (event) {
-        event.preventDefault();
-        form.appendChild(statusMessage);
+            // let request = new XMLHttpRequest();
+            // request.open('POST', 'server.php');
+            // // request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+            // request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
 
-        let request = new XMLHttpRequest();
-        request.open('POST', 'server.php');
-        // request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-        request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+            // let formData = new FormData(form);
 
-        let formData = new FormData(form);
+            // let tempObj = {};
+            // formData.forEach(function (value, key) {
+            //     tempObj[key] = value;
+            // });
+            // let json = JSON.stringify(tempObj);
 
-        let tempObj = {};
-        formData.forEach(function (value, key) {
-            tempObj[key] = value;
-        });
-        let json = JSON.stringify(tempObj);
+            // request.send(json);
 
-        request.send(json);
+            // request.addEventListener('readystatechange', function () {
+            //     if (request.readyState < 4) {
+            //         statusMessage.innerHTML = message.loading;
+            //     } else if (request.readyState === 4 && request.status == 200) {
+            //         statusMessage.innerHTML = message.success;
+            //     } else {
+            //         statusMessage.innerHTML = message.failure;
+            //     }
+            // });
+            // for (let i = 0; i < input.length; i++) {
+            //     input[i].value = '';
+            // }
 
-        request.addEventListener('readystatechange', function () {
-            if (request.readyState < 4) {
-                statusMessage.innerHTML = message.loading;
-            } else if (request.readyState === 4 && request.status == 200) {
-                statusMessage.innerHTML = message.success;
-            } else {
-                statusMessage.innerHTML = message.failure;
+            let formData = new FormData(form);
+
+            function postData(data) {
+
+                return new Promise(function (resolve, reject) {
+                    let request = new XMLHttpRequest();
+
+                    request.open('POST', 'server.php');
+
+                    request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+                    request.onreadystatechange = function () {
+                        if (request.readyState < 4) {
+                            resolve()
+                        } else if (request.readyState === 4 ) {
+                            if (request.status == 200 && request.status < 300) {
+                                resolve()
+                            } else {
+                                reject()
+                            }
+                        }
+                    }
+
+                    request.send(data);
+                })
+            } //end PostData
+
+            function clearInput() {
+                for (let i = 0; i < input.length; i++) {
+                    input[i].value = '';
+                }
             }
+
+            postData(formData)
+                .then(() => statusMessage.innerHTML = message.loading)
+                .then(() => statusMessage.innerHTML = message.success)
+                .catch(() => statusMessage.innerHTML = message.failure)
+                .then(clearInput())
         });
-        for (let i = 0; i < input.length; i++) {
-            input[i].value = '';
-        }
-    });
+    }
+    sendForm(form);
+
+
     //#endregion Form
 
 });

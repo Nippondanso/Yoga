2fee8f124d9d5686e46e36cbae699e694e708a5f
Maksym Kopaiev <mkopaev@gmail.com>
2020-03-11 23:19:10 +0200

    тест

--- js/scripts.js
+++ js/scripts.js
@@ -248,7 +248,7 @@ window.addEventListener('DOMContentLoaded', function () {
 
     dotsWrap.addEventListener('click', function (event) {
         for (let i = 0; i < dots.length + 1; i++) {
-            if (event.target.classList.contains('dot') && event.target == dots[i - 1]){
+            if (event.target.classList.contains('dot') && event.target == dots[i - 1]) {
                 currentSlide(i);
             }
         }
@@ -257,6 +257,45 @@ window.addEventListener('DOMContentLoaded', function () {
 
     //Calculator
     //#region CALC
-        
+    let persons = document.querySelectorAll('.counter-block-input')[0],
+        restDays = document.querySelectorAll('.counter-block-input')[1],
+        place = document.getElementById('select'),
+        totalValue = document.getElementById('total'),
+        personsSum = 0,
+        daysSum = 0,
+        total = 0;
+
+    totalValue.innerHTML = 0;
+
+    persons.addEventListener('change', function () {
+        personsSum = +this.value;
+        total = ((daysSum + personsSum) * 1500);
+
+        if (restDays.value == '') {
+            totalValue.innerHTML = 0;
+        } else {
+            totalValue.innerHTML = total;
+        } 
+    });
+
+    restDays.addEventListener('change', function () {
+        daysSum = +this.value;
+        total = ((daysSum + personsSum) * 1500);
+
+        if (persons.value == '') {
+            totalValue.innerHTML = 0;
+        } else {
+            totalValue.innerHTML = total;
+        }
+    });
+
+    place.addEventListener('change', function () {
+        if (restDays.value == '' || persons.value == '') {
+            totalValue.innerHTML = 0;
+        } else {
+            let a = total;
+            totalValue.innerHTML = a * this.options[this.selectedIndex].value
+        }
+    });
     //#endregion CALC
 });
